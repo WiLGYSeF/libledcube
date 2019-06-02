057884823734dafc89969b6ad8c7036d34d7a416
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2019-06-02 01:20:19 -0400

    moved project-specific pins out of general library

--- examples/led_cube/led_cube.ino
+++ examples/led_cube/led_cube.ino
@@ -1,6 +1,21 @@
 #include <libledcube.h>
 #include <charmaps/chm_shape.h>
 
+//controls
+#define _PANIMITER 23
+
+//status pins
+#define STATCOUNT 5
+#define _PSTAT0 20
+#define _PSTAT1 19
+#define _PSTAT2 18
+#define _PSTAT3 17
+#define _PSTAT4 16
+
+//random pins
+#define _PRAND1 14
+#define _PRAND2 15
+
 #define ANIMATIONS 10
 
 using namespace ledcube;
--- pins.h
+++ pins.h
@@ -13,21 +13,7 @@
 #define _PND 3
 #define _PNE 4
 
-//controls
-#define _PANIMITER 23
-
-//status pins
-#define STATCOUNT 5
-#define _PSTAT0 20
-#define _PSTAT1 19
-#define _PSTAT2 18
-#define _PSTAT3 17
-#define _PSTAT4 16
-
-//random pins
-#define _PRAND1 14
-#define _PRAND2 15
-
+//array of y pins
 const uint8_t PIN_YARR[] = {_PNA, _PNB, _PNC, _PND, _PNE};
 
 #endif //ndef __PINS_H
